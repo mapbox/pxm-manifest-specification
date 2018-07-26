9235a3bac9e68ef1b4d377c0c31027ddcd58f7e5
Norman Barker <norman.barker@mapbox.com>
2018-07-26 18:00:28 -0500

    removed asserts and formatted strings

--- manifest.py
+++ manifest.py
@@ -31,35 +31,49 @@ import click
 
 version = "0.5.0"
 
+
 # Custom click input types
-class CustomType():
+class CustomType(object):
+
     class BdxParamType(click.ParamType):
-        """Band Index Type
-        """
+        """Band Index Type"""
+
         name = 'str'
 
         def convert(self, value, param, ctx):
             try:
                 bands = [int(x) for x in value.split(',')]
-                assert len(bands) in (3, 4)
-                assert all(b > 0 for b in bands)
-                return bands
-            except (AttributeError, AssertionError):
-                raise click.ClickException('bidx must be a string with 3 or 4 ints comma-separated, '
-                                           'representing the band indexes for R,G,B(,A)')
+                if len(bands) in (3, 4):
+                    if all(b > 0 for b in bands):
+                        return bands
+                    else:
+                        raise click.ClickException('bands must be a '
+                                                   'positive number')
+                else:
+                    raise click.ClickException('band array length '
+                                               'can be 3 or 4')
+            except (AttributeError):
+                raise click.ClickException('bidx must be a string with 3 or 4 '
+                                           'ints comma-separated, '
+                                           'representing the band indexes '
+                                           'for R,G,B(,A)')
 
     class TilesetParamType(click.ParamType):
         """Tileset Type
-        The tileset must be in a form of "{account}.{id}" with a maximum of 32 chars for each.
+
+        The tileset must be in a form of "{account}.{id}" with a maximum
+        of 32 chars for each.
+
         """
         name = 'str'
 
         def convert(self, value, param, ctx):
-            try:
-                assert re.match(r"^[a-z0-9-_]{1,32}\.[a-z0-9-_]{1,32}$", value)
+            if re.match(r"^[a-z0-9-_]{1,32}\.[a-z0-9-_]{1,32}$", value):
                 return value
-            except (AssertionError):
-                raise click.ClickException('layers must follow the {account}.{id} naming (each with 32 chars max)')
+            else:
+                raise click.ClickException('layers must follow the {account}.'
+                                           '{id} naming (each with 32 chars '
+                                           'max)')
 
     class DateParamType(click.ParamType):
         """Date Type
@@ -68,18 +82,23 @@ class CustomType():
         name = 'str'
 
         def convert(self, value, param, ctx):
+            err_msg = 'date must be in either {YYYY} or {YYYY-MM-DD} format'
             try:
-                assert re.fullmatch('^[0-9]{4}|[0-9]{4}-[0-9]{2}-[0-9]{2}$', value)
+                if not re.fullmatch('^[0-9]{4}|[0-9]{4}-[0-9]{2}-[0-9]{2}$',
+                                    value):
+                    raise click.ClickException(err_msg)
 
                 if re.fullmatch('^[0-9]{4}$', value):
-                    assert datetime.datetime.strptime(value, '%Y')
+                    if not datetime.datetime.strptime(value, '%Y'):
+                        raise click.ClickException(err_msg)
 
                 if re.fullmatch('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', value):
-                    assert datetime.datetime.strptime(value, '%Y-%m-%d')
+                    if not datetime.datetime.strptime(value, '%Y-%m-%d'):
+                        raise click.ClickException(err_msg)
 
                 return value
-            except (AssertionError, ValueError):
-                raise click.ClickException('date must be in either {YYYY} or {YYYY-MM-DD} format')
+            except (ValueError):
+                raise click.ClickException(err_msg)
 
     class NdvParamType(click.ParamType):
         """No Data Value
@@ -90,8 +109,11 @@ class CustomType():
         def convert(self, value, param, ctx):
             try:
                 ndv = [int(x) for x in value.split(',')]
-                assert len(ndv) == 3
-                assert all(v > 0 for v in ndv)
+                if not len(ndv) == 3:
+                    raise click.ClickException('ndv must be 3 values')
+
+                if not all(v > 0 for v in ndv):
+                    raise click.ClickException('ndv must be greater than zero')
                 return ndv
             except (AttributeError, TypeError):
                 raise click.ClickException('ndv must be a string with ints '
@@ -104,11 +126,9 @@ class CustomType():
         name = 'str'
 
         def convert(self, value, param, ctx):
-            try:
-                assert value.startswith('EPSG:')
-                return value
-            except (AssertionError):
+            if not value.startswith('EPSG:'):
                 raise click.ClickException('crs string must start with EPSG')
+            return value
 
     class MbxAccountParamType(click.ParamType):
         """Mapbox Account
@@ -117,11 +137,10 @@ class CustomType():
         name = 'str'
 
         def convert(self, value, param, ctx):
-            try:
-                assert re.match(r"^[a-z0-9-_]{1,32}$", value)
-                return value
-            except (AssertionError):
-                raise click.ClickException('Mapbox account name must have 32 () chars max and no blank space')
+            if not re.match(r"^[a-z0-9-_]{1,32}$", value):
+                raise click.ClickException('Mapbox account name must have 32'
+                                           '() chars max and no blank space')
+            return value
 
     bdx = BdxParamType()
     tileset = TilesetParamType()
@@ -214,3 +233,4 @@ def create_manifest(sources, tileset, license, account, product, date, notes,
 
 if __name__ == "__main__":
     create_manifest()
+
